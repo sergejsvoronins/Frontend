365d7965107b6e3baeab12cbb10291c6cfe78037
sergejsvoronins <svoronins@gmail.com>
2022-10-18 09:05:28 +0200

    structure is changed

---
+++ Övningar/JS/Beginning/src/js/main.js
@@ -0,0 +1,28 @@
+function myfirstjavascript () {
+  let result = confirm("Är du säker?");
+    console.log(result);
+}
+   
+const name = "Sergejs";
+console.log(name)   ;
+
+const x = 5;
+const y = 10;
+console.log(x+y);
+
+function addClass(){
+    const element = document.getElementById("text");
+    element.className += "red";
+
+    element.innerHTML="Hej på dig";
+    const elementOne = document.getElementById("content");
+    elementOne.id = "wohoo";   
+}
+
+
+
+
+
+
+
+

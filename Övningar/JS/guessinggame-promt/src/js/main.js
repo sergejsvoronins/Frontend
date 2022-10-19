c843a6fbaecf53fa182beebec4d8842619938118
sergejsvoronins <svoronins@gmail.com>
2022-10-19 21:01:09 +0200

    added IF statement

--- Övningar/JS/guessinggame-promt/src/js/main.js
+++ Övningar/JS/guessinggame-promt/src/js/main.js
@@ -1,7 +1,5 @@
 let answerEl = document.getElementById("answer");
 
-
-
 function guessingFunction(){
     let inputOne = prompt("Ange ett nummer mellan 0 och 9");
     let inputNum = Number(inputOne);
@@ -11,19 +9,21 @@ function guessingFunction(){
         let wrongInput = prompt("Du har angett fel nummer. Försök igen!");
         inputNum = Number(wrongInput);
     }
+
     confirm("Är u redo att gissa?");
 
     let inputTwo = prompt("Gissa numret");
     let guessNum = Number(inputTwo);
     
     while((inputNum!==guessNum)){
-
-
-        
-        let a = prompt("Gissa vidare");
-        guessNum =Number(a);
-        answerEl.innerHTML = "Fel";
-        
+        if (guessNum>inputNum){
+            let a = prompt("För stort. Gissa vidare");
+            guessNum =Number(a);
+        }
+        else {
+        let b = prompt("För lågt. Gissa vidare");
+        guessNum =Number(b);    
+        }  
     }
     answerEl.innerHTML="Du har gissat rätt";
 }
